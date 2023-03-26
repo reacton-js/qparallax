bde45fd08be3724f12f1ac83412a2ac225f09161
reacton-js <reacton@bk.ru>
2023-03-27 01:26:00 +0300

    update

--- qparallax/dist/qparallax.js
+++ qparallax/dist/qparallax.js
@@ -1,13 +1,21 @@
 'use strict'
 
 !function() {
+  // определить хранилище обратных вызовов
   const callbacks = new Set()
 
+  // перебрать все параллаксы
   for (const qparallax of document.querySelectorAll('.qparallax')) {
+    // определить скорость параллакса
     const speed = qparallax.dataset.speed || 2
+
+    // определить стиль параллакса
     const style = qparallax.querySelector('.qparallax__lower').style
+
+    // добавить обратный вызов в хранилище
     callbacks.add(() => style.setProperty('--offset', window.scrollY * speed))
   }
 
+  // при прокрутке окна, вызвать обратные вызовы из хранилища
   window.addEventListener('scroll', () => callbacks.forEach(cb => cb()))
 }()
